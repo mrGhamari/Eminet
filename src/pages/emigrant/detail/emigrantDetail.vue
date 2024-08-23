eb36c4ec80ff593cb7eac62b92b5ab8ae6e5b2e5
mrGhamari <mmdrza77@gmail.com>
2024-08-23 17:08:10 +0330

    create detail emigrant detail page

---
+++ src/pages/emigrant/detail/emigrantDetail.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="emigrant-detail-page"></q-page>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'EmigrantDetail' });
+</script>
+
+<style scoped></style>
