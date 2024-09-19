37d295566537e4eb049cc17c4fb0282eee234359
mrGhamari <mmdrza77@gmail.com>
2024-09-19 17:13:01 +0330

    load custom icon

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
 
 <script setup lang="ts">
 defineOptions({ name: 'App' });
+import { useIcon } from 'src/composables/use-icon';
 import { useLocale } from 'src/composables/use-locale';
 
+// ------ Variables ------
+const icon = useIcon();
 const locale = useLocale();
 
 // ------ Methods ------
+icon.loadIcon();
 locale.loadLastState();
 </script>
